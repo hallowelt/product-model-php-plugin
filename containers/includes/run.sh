373a166c5cfb1d2f50c947ed869b356869969941
Markus Glaser <glaser@hallowelt.com>
2021-05-05 14:28:06 +0200

    Make source and output dir mountable

--- containers/includes/run.sh
+++ containers/includes/run.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 export NODE_TLS_REJECT_UNAUTHORIZED=0
-cd /opt/workspace
-rm -rf *
 
 if [ $USE_DEFAULT_REPO -eq 1 ]; then
-	git clone https://github.com/wikimedia/mediawiki-extensions-BlueSpiceFoundation.git /opt/workspace/target
+	cd /source
+	rm -rf *
+	git clone https://github.com/wikimedia/mediawiki-extensions-BlueSpiceFoundation.git /source
 fi
-cd /opt/workspace/target
+cd /source
 composer update --ignore-platform-reqs
 
-cd /opt/workspace
-git clone https://github.com/hallowelt/product-model-php-plugin.git /opt/workspace/scanner
-cd /opt/workspace/scanner
+cd /opt/scripts
+git clone https://github.com/hallowelt/product-model-php-plugin.git /opt/scripts/scanner
+cd /opt/scripts/scanner
 composer update --ignore-platform-reqs
 
 cp config.template.json config.json
-php phpScanner.php /opt/workspace/target
+php phpScanner.php --sourcedir=/source --outputdir=/output
